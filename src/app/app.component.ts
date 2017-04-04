66e4ebaf0588a500f27f8bce3ab172ffaf7abdbb
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-04-04 14:59:14 +0530

    added new files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  employees = [{
+    name: 'Rajesh',
+    contact: '2452452',
+    address: 'avf fadsf adfadf'
+  },
+  {
+    name: 'Santhosh',
+    contact: '12124',
+    address: 'aaa ffg dfgdfh'
+  },
+  {
+    name: 'Mahesh',
+    contact: '56858',
+    address: 'ddgg asda'
+  }
+  ]
 }
